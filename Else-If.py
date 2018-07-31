769da4edc0fea94fce6dd8c6cf3aee410545317c
Suraj Kanojiya <suraj.kanojiya@biz2credit.org>
2018-07-31 16:18:28 +0530

    Else If Added

---
+++ Else-If.py
@@ -0,0 +1,12 @@
+# To Test if else statement
+x = int(input('Please Enter a Number :\n'))
+print(x)
+
+if (x<0):
+    print('Number is negative')
+elif(x==0):
+    print('Number is Zero')
+elif(x==1):
+    print('Number is single')
+else:
+    print('Number is greater than one')
---
+++ Fibbonaci.py
@@ -0,0 +1,8 @@
+#Print Fibbonaci
+a,b = 0,1
+while(a<10):
+    #print('***********'+str(a) +"***********"+str(b))
+    print(a,end=' ')
+    a,b = b,a+b
+    #print('-----------'+str(a) +"-----------"+str(b))
+    #print("\n")
