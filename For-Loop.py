5da1c80982441898450a976f2f065864478d1eb4
Suraj Kanojiya <suraj.kanojiya@biz2credit.org>
2018-07-31 16:35:52 +0530

    For loop

---
+++ For-Loop.py
@@ -0,0 +1,10 @@
+# Using for loop
+words = ['hello','world','fibbonaci','The quick brown fox']
+for w in words:
+    print("Length of \""+ w + "\" is " +str(len(w)))
+
+
+# Using Range 
+
+for i in range(6):
+    print(i)
